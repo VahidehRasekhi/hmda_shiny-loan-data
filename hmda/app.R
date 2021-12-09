a830cf1fe3ab2c1b87075d6e1ab2e5549d09efb8
George Mills <george.r.mills@gmail.com>
2021-12-08 21:19:22 -0600

    Loaded in json with institution names

---
+++ hmda/app.R
@@ -0,0 +1,54 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+library(httr)
+
+url <- 'https://ffiec.cfpb.gov/v2/data-browser-api/view/filers?states=WA&years=2020'
+response <- GET(url) 
+lei <- content(response, type = 'text') %>% 
+  jsonlite::fromJSON() %>% 
+  .[[1]] %>%                      # Select the first element of the resulting list
+  as_tibble()
+
+# data <- read.csv("data/state_WA.csv")
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("HMDA Loan Data"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+          selectInput("select", label = h3("Select box"), 
+                      choices = lei$name, 
+                      selected = 1),
+          
+          
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+          fluidRow(column(3, verbatimTextOutput("value")))
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+      output$value <- renderPrint({ input$select })
+
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
--- learn.R
+++ learn.R
@@ -8,4 +8,41 @@ count(data, lei)
 
 library("rjson")
 result <- fromJSON(file ="data/lei_name.json")
-json_data_frame <- as.data.frame(result)
+
+json_data_frame <- as.data.frame(fromJSON(file ="data/lei_name.json"))
+lei_list <- as.list(fromJSON(file ="data/lei_name.json"))
+
+lei_num <- "549300ILXET2VTO1HD84"
+
+one_instit <- filter(data, lei == lei_num)
+
+data %>% filter(lei == lei_num) %>% select(action_taken)
+
+lei_list <- as.list(json_data_frame)
+
+tmp <- filter(lei_tib, lei == lei_num) 
+tmp$name 
+
+lei_nums <- lei_list[seq(1, length(lei_list) - 1, 4)]
+lei_names <- lei_list[seq(2, length(lei_list), 4)]
+lei_tib <- tibble(lei = unlist(unname(lei_nums)), name = unlist(unname(lei_names)))
+
+lei_tib
+
+lei_tib %>% slice(3)
+
+tmp <- lei_list[seq(1, length(lei_list) - 1, 4)]
+unlist(unname(tmp))
+
+
+library(httr)
+library(tidyverse)
+
+url <- 'https://ffiec.cfpb.gov/v2/data-browser-api/view/filers?states=WA&years=2020'
+response <- GET(url) 
+lei <- content(response, type = 'text') %>% 
+  jsonlite::fromJSON() %>% 
+  .[[1]] %>%                      # Select the first element of the resulting list
+  as_tibble()
+lei
+
