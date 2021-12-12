53f17c09bc6e4e9bfd30a2fe49562382c9c29782
George Mills <george.r.mills@gmail.com>
2021-12-12 17:58:11 -0600

    Select input for action_taken and key lookup implemented.

--- hmda/app.R
+++ hmda/app.R
@@ -30,16 +30,18 @@ ui <- fluidPage(
             selectInput("select", label = h3("Select box"), 
                         choices = lei_names$name, 
                         selected = 1),            
-            selectInput("select", label = h3("Select box"), 
-                        choices = lei_names$name,
-                        selected = 1),
+            selectInput("action_taken", label = h3("Select box"), 
+                        choices = c("All", lars_names %>% 
+                                        filter(lars_names == "action_taken") %>% pull(value)),
+                        selected = 1)
             
             
         ),
         
         # Show a plot of the generated distribution
         mainPanel(
-            fluidRow(column(10, verbatimTextOutput("value")))
+            fluidRow(column(10, verbatimTextOutput("value1"))),
+            fluidRow(column(10, verbatimTextOutput("value2")))
             # ggplot("plot")
         )
     )
@@ -48,7 +50,12 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     
-    output$value <- renderPrint({ input$select })
+    output$value1 <- renderPrint({ input$select })
+    
+    output$value2 <- renderPrint({ lars_names %>% 
+            filter(lars_names == "action_taken", value == input$action_taken) %>% pull(key) })
+    
+    
     
     # renderPlot(date %>% select(input$select))
     
--- parse_data.R
+++ parse_data.R
@@ -1,4 +1,16 @@
 library(tidyverse)
+library(kit)
 
 data <- read.csv("data/state_WA.csv")
+lars_names <- read.csv("data/lars_lookup.csv")
 
+data$action_taken
+
+colnames(data)
+
+unique(data$derived_sex)
+
+lars_names %>% filter(lars_names == "action_taken") %>% pull(value)
+
+selected <- "File closed for incompleteness"
+lars_names %>% filter(lars_names == "action_taken", value == selected) %>% pull(key)
