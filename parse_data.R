d6dddfcf2b3e6c9d14babbbb8ff4e65080a55069
George Mills <george.r.mills@gmail.com>
2021-12-12 22:03:02 -0600

    Started on updated lookup csv file

--- parse_data.R
+++ parse_data.R
@@ -12,4 +12,63 @@ unique(data$derived_sex)
 lars_names %>% filter(lars_names == "action_taken") %>% pull(value)
 
 selected <- "File closed for incompleteness"
-lars_names %>% filter(lars_names == "action_taken", value == selected) %>% pull(key)
+lars_names %>% 
+  filter(lars_names == "action_taken", value == selected) %>% 
+  pull(key)
+
+
+
+###############################################################################
+data <- read_lines("data/lar_data_fields.txt")
+lars_names <- c()
+options <- c()
+for (i in 1:length(data)){
+  if (grepl( "^1 - ", data[i],)) {
+    print(data[i - 3])
+    j <- 0
+    while (grepl( " - ", data[i + j],)) {
+      options <- c(options, data[i + j])
+      lars_names <- c(lars_names, data[i - 3])
+      j <- j + 1
+    }
+  }
+}
+lars_tib <- tibble(lars_names = lars_names, options = options)
+lars_tib <- lars_tib %>% 
+  separate(options, c("key", "value"), " - ", extra = "merge")
+write_csv(lars_tib, "data/lars_lookup.csv")
+###############################################################################
+
+data <- read_lines("data/lar_data_fields.txt")
+lars_names <- c()
+options <- c()
+for (i in 1:length(data)) {
+  if (grepl("Values:", data[i],)) {
+    # works every time except line 67 where 0 starts the option list
+    if (grepl("^1 - ", data[i+1],)) {
+      print(data[i - 2])
+      j <- 1
+      while (grepl(" - ", data[i + j],)) {
+        options <- c(options, data[i + j])
+        lars_names <- c(lars_names, data[i - 2])
+        j <- j + 1
+      }
+    }
+  }
+}
+lars_tib <- tibble(lars_names = lars_names, options = options)
+lars_tib <- lars_tib %>% 
+  separate(options, c("key", "value"), " - ", extra = "merge")
+write_csv(lars_tib, "data/lars_lookup.csv")
+
+
+
+
+
+
+
+
+
+
+
+
