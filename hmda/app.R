1be4bc5c086b892def4b74e5950a5407bf736612
George Mills <george.r.mills@gmail.com>
2021-12-14 19:17:07 -0600

    added another selectInput

--- hmda/app.R
+++ hmda/app.R
@@ -1,65 +1,27 @@
-#
-# HMDA Shiny Load Data Application
-#
-# Pull down for selecting institution, typing in the input field will 
-#   filter the items in the selection
-#
-#
-#
-#
-#
-#
-
 library(shiny)
-library(tidyverse)
-library(httr)
+library(leaflet)
 
-# data <- read.csv("data/state_WA.csv")
-lei_names <- read.csv("data/lei_name.csv")
-lars_names <- read.csv("data/lars_lookup.csv")
+r_colors <- rgb(t(col2rgb(colors()) / 255))
+names(r_colors) <- colors()
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-    
-    # Application title
-    titlePanel("HMDA Loan Data"),
-    
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            selectInput("select", label = h3("Select box"), 
-                        choices = lei_names$name, 
-                        selected = 1),            
-            selectInput("action_taken", label = h3("Select box"), 
-                        choices = c("All", lars_names %>% 
-                                        filter(lars_names == "action_taken") %>% pull(value)),
-                        selected = 1)
-            
-            
-        ),
-        
-        # Show a plot of the generated distribution
-        mainPanel(
-            fluidRow(column(10, verbatimTextOutput("value1"))),
-            fluidRow(column(10, verbatimTextOutput("value2")))
-            # ggplot("plot")
-        )
-    )
+  leafletOutput("mymap"),
+  p(),
+  actionButton("recalc", "New points")
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-    
-    output$value1 <- renderPrint({ input$select })
-    
-    output$value2 <- renderPrint({ lars_names %>% 
-            filter(lars_names == "action_taken", value == input$action_taken) %>% pull(key) })
-    
-    
-    
-    # renderPlot(date %>% select(input$select))
-    
+server <- function(input, output, session) {
+  
+  points <- eventReactive(input$recalc, {
+    cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
+  }, ignoreNULL = FALSE)
+  
+  output$mymap <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles(providers$Stamen.TonerLite,
+                       options = providerTileOptions(noWrap = TRUE)
+      ) %>% addMarkers(data = points())
+  })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
---
+++ tmp.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+
+data <- read.csv("data/state_WA.csv")
+
+data$action_taken
+
+colnames(data)
+
+unique(data$derived_sex)
+
+lars_names %>% filter(lars_names == "action_taken") %>% pull(value)
+
+selected <- "File closed for incompleteness"
+lars_names %>% 
+  filter(lars_names == "action_taken", value == selected) %>% 
+  pull(key)
+
+
+
+data %>% filter(lei == "549300HW662MN1WU8550") %>% pull(census_tract)
+  
+
+
+
+
+
+
